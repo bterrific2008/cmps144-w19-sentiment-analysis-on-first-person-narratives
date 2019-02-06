f15d7ddf95507769a07f3987318e1cff7268d87c
Bryan Tor <bterrific2008@gmail.com>
2019-02-06 01:24:53 -0800

    Created a csv file with all of our data

--- data-to-pandas.py
+++ data-to-pandas.py
@@ -3,27 +3,35 @@ from hidden import filePath
 import numpy as np
 import pandas as pd
 
+idPhrase = []
+sentiment = []
+
 # obtain all instances of development data with negative sentiment
-fileNames = os.listdir(filePath + "dev/neg/")
+fileNames = os.listdir(filePath + "train/neg/")
 negSentences = []
 for i in range(1, len(fileNames)):
-    file = open(filePath + "dev/neg/" + fileNames[i], 'r')
+    file = open(filePath + "train/neg/" + fileNames[i], 'r')
     negSentences.append(file.read())
+    idPhrase.append(fileNames[i])
+    sentiment.append("0")
 print(len(negSentences))
 
 # turn the assembled array of negative sentences into a pandas serries
 negSeries = pd.Series(negSentences)
 
 # obtain all instances of dev data with positive sentiment
-fileNames = os.listdir(filePath + "dev/pos/")
+fileNames = os.listdir(filePath + "train/pos/")
 posSentences = []
 for i in range(1, len(fileNames)):
-    file = open(filePath + "dev/pos/" + fileNames[i], 'r')
+    file = open(filePath + "train/pos/" + fileNames[i], 'r')
     posSentences.append(file.read())
+    idPhrase.append(fileNames[i])
+    sentiment.append("1")
 print(len(posSentences))
 
 # assembles everything into a Pandas dataframe
-d = {'neg': pd.Series(negSentences), 'pos': pd.Series(posSentences)}
+d = {'id': pd.Series(idPhrase), 'sentence': pd.Series(negSentences+posSentences), 'sentiment': pd.Series(sentiment)}
 df = pd.DataFrame(d)
 print(df.index)
 print(df.columns)
+df.to_csv("sentences.csv")
